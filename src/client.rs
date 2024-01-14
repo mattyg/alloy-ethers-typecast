4a31fbdf5727c1a05a8710e3d1a6f10889a43c26
Matt Gabrenya <matt@buildyourweb.app>
2024-01-14 16:11:14 -0700

    feat: use blocknative gas oracle

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,10 @@
-use ethers::middleware::gas_oracle::{GasOracleMiddleware, ProviderOracle};
+use ethers::middleware::gas_oracle::{BlockNative, GasCategory, GasOracleMiddleware};
 use ethers::middleware::{MiddlewareBuilder, SignerMiddleware};
 use ethers::prelude::{Http, Provider};
 use ethers_signers::{HDPath, Ledger};
 
 pub struct LedgerClient {
-    pub client: SignerMiddleware<
-        GasOracleMiddleware<Provider<Http>, ProviderOracle<Provider<Http>>>,
-        Ledger,
-    >,
+    pub client: SignerMiddleware<GasOracleMiddleware<Provider<Http>, BlockNative>, Ledger>,
 }
 
 impl LedgerClient {
@@ -15,16 +12,22 @@ impl LedgerClient {
         ledger_derivation_path: Option<usize>,
         chain_id: u64,
         rpc_url: String,
+        gas_priority: Option<GasCategory>,
     ) -> anyhow::Result<Self> {
         let wallet = Ledger::new(
             HDPath::LedgerLive(ledger_derivation_path.unwrap_or(0)),
             chain_id,
         )
         .await?;
+
+        let api_key: Option<String> = std::env::var("BLOCK_NATIVE_API_KEY").ok();
+        let blocknative =
+            BlockNative::new(api_key).category(gas_priority.unwrap_or(GasCategory::Standard));
+
         let provider = Provider::<Http>::try_from(rpc_url.clone())?;
         let client = provider
             .clone()
-            .wrap_into(|s| GasOracleMiddleware::new(s, ProviderOracle::new(provider)))
+            .wrap_into(|s| GasOracleMiddleware::new(s, blocknative))
             .with_signer(wallet);
 
         Ok(Self { client })
